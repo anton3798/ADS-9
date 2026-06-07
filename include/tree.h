e29c6dd6ccb6e1e8e286976708e3475a26e7ca44
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-06-07 21:35:31 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,35 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <string>
+
+struct Node {
+    char value;
+    std::vector<Node*> children;
+    Node(char val) : value(val) {}
+};
+class PMTree {
+ private:
+    Node* root;
+    std::vector<char> alphabet;
+    void buildTree(Node* parent, std::vector<char> remaining);
+    void deleteTree(Node* node);
+    void collectPermutations(Node* node, std::vector<char>& current,
+        std::vector<std::vector<char>>& result);
+
+ public:
+    PMTree(const std::vector<char>& input);
+    ~PMTree();
+    Node* getRoot() const { return root; }
+    std::vector<char> getAlphabet() const { return alphabet; }
+    friend std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+    friend std::vector<char> getPerm1(const PMTree& tree, int num);
+    friend std::vector<char> getPerm2(const PMTree& tree, int num);
+};
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(const PMTree& tree, int num);
+std::vector<char> getPerm2(const PMTree& tree, int num);
+int factorial(int n);
+
 #endif  // INCLUDE_TREE_H_
